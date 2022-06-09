af8a29cf3e58ce8d7cb8c2a091feec52c510d8f9
GuiTx <86571679+inputGui@users.noreply.github.com>
2022-06-08 22:01:48 -0400

    changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import "./About.css";
+import me from "../assets/me.jpg";
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+    <div class="split left">
+      <div className="centered">
+        <img 
+          className="profile_image"
+          src={me}
+          alt="Profile Pic"
+          ></img>
       </div>
+    </div>
+    <div className="split right">
+      <div className="centered">
+        <div className="name_title">Guilherme Noronha Rigaud</div>
+        <div className="brief_description">
+          It is Me, Mario
+        </div>
+      </div>
+    </div>
+  </div>
     )
   }
 }
